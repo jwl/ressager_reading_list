e0e8dd98d3a39571570138a7d875d912c343b963
jwl <james.warren.leung@gmail.com>
2018-06-28 22:56:30 -0700

    Finished up to end of Lesson 12

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 Based on tutorial at: https://www.youtube.com/watch?=v=ed8SzALpx1Q
+
 Sample files: https://github.com/iamshaunjp/graphql-playlist
 
 
---
+++ server/schema/schema.js
@@ -0,0 +1,71 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { 
+  GraphQLObjectType, 
+  GraphQLString, 
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql;
+
+// dummy data
+var dummy_book_data = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3'},
+  { name: 'The Fifth Season', genre: 'Sci-Fi', id: '4'},
+];
+
+var dummy_author_data = [
+  {name: 'Patrick Rothfuss', age: 44, id: '1'},
+  {name: 'Brandon Sanderson', age: 42, id: '2'},
+  {name: 'Terry Pratchett', age: 66, id: '3'},
+  {name: 'N.K. Jemisin', age: 45, id: '4'},
+];
+
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    get_me_a_book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // code to get data from db / other source
+        // use args.id
+        //console.log(typeof(args.id));
+        return _.find(dummy_book_data, { id: args.id });
+      }
+    },
+    get_me_an_author: {
+      type: AuthorType,
+      args: {id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(dummy_author_data, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
