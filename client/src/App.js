8c8619e72941849c423cc490530ae60e2e648d9e
jleung <jleung@riotgames.com>
2018-07-21 17:50:11 -0700

    Created initial frontend, updated to end of Lesson 23

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+      <h1>My Reading List</h1>
+
+      </div>
+    );
+  }
+}
+
+export default App;
