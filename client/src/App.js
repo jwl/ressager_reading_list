45740c6d7d915e1fd21717d2731994189d00d1ca
jleung <jleung@riotgames.com>
2018-07-22 12:42:32 -0700

    Updated to end of Lesson 24

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+// components
+import BookList from './components/BookList'
+
 class App extends Component {
   render() {
     return (
       <div id="main">
-      <h1>My Reading List</h1>
-
+        <h1>My Reading List</h1>
+        <BookList />
       </div>
     );
   }
