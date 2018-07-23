86e2a343d9f0d59addafe53fe8815ae6b8b540ac
jleung <jleung@riotgames.com>
2018-07-22 18:36:14 -0700

    Updated to end of Lesson 25

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 // components
 import BookList from './components/BookList'
 
+// apollo client setup
+const apollo_client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>My Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={apollo_client}>
+        <div id="main">
+          <h1>My Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
