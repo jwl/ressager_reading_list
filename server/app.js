f0d1d2a448eebe6892cd764e0318312c4694929b
jleung <jleung@riotgames.com>
2018-07-22 18:50:19 -0700

    Updated to end of Lesson 26

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 // connect to mlab database
 mongoose.connect('mongodb://jwl:test123@ds123981.mlab.com:23981/ressager_reading_list')
