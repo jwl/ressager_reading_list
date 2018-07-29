e3c958e71dc8de43a148531348aaae05a015a2e5
jwl <james.warren.leung@gmail.com>
2018-07-29 10:44:10 -0700

    Updated to end of Lesson 34

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -29,4 +29,24 @@ const addBookMutation = gql`
   }
 `;
 
-export { getAuthorsQuery, getBooksQuery, addBookMutation };
+const getBookQuery = gql`
+  query($id: ID) {
+    get_me_a_book(id: $id) {
+      id
+      name
+      genre
+      author {
+        id
+        name
+        age
+        books {
+          name
+          id
+        }
+      }
+    }
+  }
+`
+
+
+export { getAuthorsQuery, getBooksQuery, addBookMutation, getBookQuery };
